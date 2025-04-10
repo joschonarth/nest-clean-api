18df98a94801b379a06fff4f8f44aa8d66781f30
joschonarth <joschonarth@gmail.com>
2025-04-10 18:54:28 -0300

    chore(e2e): increase test and hook timeouts

--- vitest.config.e2e.mjs
+++ vitest.config.e2e.mjs
@@ -8,6 +8,8 @@ export default defineConfig({
     globals: true,
     root: './',
     setupFiles: ['./test/setup-e2e.ts'],
+    testTimeout: 30000,
+    hookTimeout: 30000,
   },
   plugins: [
     tsConfigPaths(),
