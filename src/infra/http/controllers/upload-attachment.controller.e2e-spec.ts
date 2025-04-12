f4e8aacecc8cc032fdb1c09c457461ab0da3f67d
joschonarth <joschonarth@gmail.com>
2025-04-12 12:25:58 -0300

    feat: improve upload attachment controller

--- src/infra/http/controllers/upload-attachment.controller.e2e-spec.ts
+++ src/infra/http/controllers/upload-attachment.controller.e2e-spec.ts
@@ -35,6 +35,9 @@ describe('Upload attachment (E2E)', () => {
       .set('Authorization', `Bearer ${accessToken}`)
       .attach('file', './test/e2e/sample-upload.png')
 
-    expect(response.status).toBe(201)
+    expect(response.statusCode).toBe(201)
+    expect(response.body).toEqual({
+      attachmentId: expect.any(String),
+    })
   })
 })
